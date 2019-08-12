fa17c5b81fd603b70a163609f6ee8d9b7906f6b2
Logan Wilquin <lwilquin@datasolution.fr>
2019-08-12 15:05:30 +0200

    initial

---
+++ src/layout/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Loading() {
+    return(
+        <div className="Loading">
+            <svg id="rectangle" viewBox="-3 -4 39 39">
+                <polygon points="16,0 32,32 0,32">
+                </polygon>
+            </svg>
+            <p>Loading</p>
+        </div>
+    );
+};
+
+export default Loading;
